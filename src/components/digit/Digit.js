8275412ae5de685ec577ae55fd8af4126e47dc0b
Michael McCann <81657333+MichaelMcCann1@users.noreply.github.com>
2021-09-01 12:20:19 -0500

    Add files via upload

--- src/components/digit/Digit.js
+++ src/components/digit/Digit.js
@@ -4,13 +4,13 @@ import "./digit.css"
 export default function Digit({number}) {
   return (
     <div className="digit">
-      <div className={"zero two three five six seven eight nine".includes(number) ? "top active" : "top"}></div>
-      <div className={"zero one two three four seven eight nine".includes(number) ? "topRight active" : "topRight"}></div>
-      <div className={"zero one three four five six seven eight nine".includes(number) ? "bottomRight active" : "bottomRight"}></div>
-      <div className={"zero two six eight".includes(number) ? "bottomLeft active" : "bottomLeft"}></div>
-      <div className={"zero four five six eight nine".includes(number) ? "topLeft active" : "topLeft"}></div>
-      <div className={"two three four five six eight nine".includes(number) ? "middle active" : "middle"}></div>
-      <div className={"two zero three five six eight".includes(number) ? "bottom active" : "bottom"}></div>
+      <div className={[0,2,3,5,6,7,8,9].includes(number) ? "top active" : "top"}></div>
+      <div className={[0,1,2,3,4,7,8,9].includes(number) ? "topRight active" : "topRight"}></div>
+      <div className={[0,1,3,4,5,6,7,8,9].includes(number) ? "bottomRight active" : "bottomRight"}></div>
+      <div className={[0,2,6,8].includes(number) ? "bottomLeft active" : "bottomLeft"}></div>
+      <div className={[0,4,5,6,8,9].includes(number) ? "topLeft active" : "topLeft"}></div>
+      <div className={[2,3,4,5,6,8,9].includes(number) ? "middle active" : "middle"}></div>
+      <div className={[0,2,3,5,6,8].includes(number) ? "bottom active" : "bottom"}></div>
     </div>
   )
 }
